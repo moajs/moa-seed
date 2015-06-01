0c8908fd116546dd7727aeb37454896e0f6b29b6
alfred sang <i5ting@126.com>
2015-06-01 21:48:12 +0800

    use mongoosedao 1.0.4

--- app/controllers/movies_controller.js
+++ app/controllers/movies_controller.js
@@ -1,11 +1,8 @@
 /**
- * Created by san on 01/06/14.
+ * Created by sang on 01/06/14.
  */
 
 var Movie = require('../models/movie');
-// var express = require('express');
-
-//configure routes
 
 exports.list = function (req, res, next) {
   console.log(req.method + ' /movies => list, query: ' + JSON.stringify(req.query));
@@ -21,7 +18,9 @@ exports.new = function (req, res, next) {
   console.log(req.method + ' /movies/new => new, query: ' + JSON.stringify(req.query));
   
   res.render('movies/new', {
-    movie : {}
+    movie : {
+      "_action" : "new"
+    }
   })
 };
 
@@ -109,77 +108,3 @@ exports.destroy = function (req, res, next) {
     });
   });
 };
-
-
-//
-// var router = express.Router();
-//
-// exports.save = function(req,res){
-//        Movie.find(function(err,movies){
-//            if(err)
-//                 res.send(err);
-//            res.json(movies);
-//        });
-//     };
-//
-// router.route('/movies')
-//     .get(function(req,res){
-//        Movie.find(function(err,movies){
-//            if(err)
-//                 res.send(err);
-//            res.json(movies);
-//        });
-//     })
-//
-//     .post(function(req,res){
-//         var movie=new Movie(req.body);
-//         movie.save(function(err){
-//             if(err)
-//                 res.send(err);
-//             res.send({message:'Movie Added'});
-//         });
-//     });
-//
-// router.route('/movies/:id')
-//     .put(function(req,res){
-//         Movie.findOne({_id:req.params.id},function(err,movie){
-//
-//             if(err)
-//                 res.send(err);
-//
-//            for(prop in req.body){
-//                 movie[prop]=req.body[prop];
-//            }
-//
-//             // save the movie
-//             movie.save(function(err) {
-//                 if (err)
-//                     res.send(err);
-//
-//                 res.json({ message: 'Movie updated!' });
-//             });
-//
-//         });
-//     })
-//
-//     .get(function(req,res){
-//         Movie.findOne({_id:req.params.id},function(err, movie) {
-//             if(err)
-//                 res.send(err);
-//
-//             res.json(movie);
-//         });
-//     })
-//
-//     .delete(function(req,res){
-//         Movie.remove({
-//             _id: req.params.id
-//         }, function(err, movie) {
-//             if (err)
-//                 res.send(err);
-//
-//             res.json({ message: 'Successfully deleted' });
-//         });
-//     });
-//
-// module.exports=router;
