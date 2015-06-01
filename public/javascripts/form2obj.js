e3164982d7e221644767c7d34bb530e68bcc96e7
alfred sang <i5ting@126.com>
2015-06-01 21:03:19 +0800

    refact form2obj code

--- public/javascripts/form2obj.js
+++ public/javascripts/form2obj.js
@@ -1,31 +1,30 @@
-var proto = Element.prototype;
-var vendor = proto.matches
-  || proto.matchesSelector
-  || proto.webkitMatchesSelector
-  || proto.mozMatchesSelector
-  || proto.msMatchesSelector
-  || proto.oMatchesSelector;
- 
-/**
- * Match `el` to `selector`.
- *
- * @param {Element} el
- * @param {String} selector
- * @return {Boolean}
- * @api public
- */
-  
-function matches(el, selector) {
-  if (vendor) return vendor.call(el, selector);
-  var nodes = el.parentNode.querySelectorAll(selector);
-  for (var i = 0; i < nodes.length; i++) {
-    if (nodes[i] == el) return true;
-  }
-  return false;
-}
-
 function form2obj(form) {
-  var body = Object.create(null)
+  var body = Object.create(null);
+  var proto = Element.prototype;
+  var vendor = proto.matches
+    || proto.matchesSelector
+    || proto.webkitMatchesSelector
+    || proto.mozMatchesSelector
+    || proto.msMatchesSelector
+    || proto.oMatchesSelector;
+ 
+  /**
+   * Match `el` to `selector`.
+   *
+   * @param {Element} el
+   * @param {String} selector
+   * @return {Boolean}
+   * @api public
+   */
+  
+  function matches(el, selector) {
+    if (vendor) return vendor.call(el, selector);
+    var nodes = el.parentNode.querySelectorAll(selector);
+    for (var i = 0; i < nodes.length; i++) {
+      if (nodes[i] == el) return true;
+    }
+    return false;
+  }
 
   Array.prototype.slice.call(form.querySelectorAll('input, textarea, select'))
   .forEach(function (el) {
