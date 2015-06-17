5d884e84404d7a748028a3a3c19bc9ed658b2f52
alfred sang <i5ting@126.com>
2015-06-17 18:02:50 +0800

    uncaughtException来全局捕获未捕获的Error

--- app.js
+++ app.js
@@ -79,4 +79,11 @@ app.use(function(err, req, res, next) {
   });
 });
 
+process.on('uncaughtException', function (err) {
+  //打印出错误
+  console.log(err);
+  //打印出错误的调用栈方便调试
+  console.log(err.stack)；
+});
+
 module.exports = app;
