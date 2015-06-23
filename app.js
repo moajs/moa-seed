ef26153b6209b07a671c669fa1779b8bd6bba2ab
alfred sang <i5ting@126.com>
2015-06-23 10:17:13 +0800

    spell isssue

--- app.js
+++ app.js
@@ -83,7 +83,7 @@ process.on('uncaughtException', function (err) {
   //打印出错误
   console.log(err);
   //打印出错误的调用栈方便调试
-  console.log(err.stack)；
+  console.log(err.stack);
 });
 
 module.exports = app;
