2f9eda4282ad71af63676326a07004052ffba2d0
alfred sang <i5ting@126.com>
2015-08-01 16:53:01 +0800

    refact Express to Moajs

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,7 +5,7 @@ var $middlewares = require('mount-middlewares')(__dirname);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Moajs' });
 });
 
 module.exports = router;
