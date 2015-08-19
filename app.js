e4602eef007edd938e05e5b42c1294a0e9ec9164
i5ting <i5ting@126.com>
2015-08-19 11:56:09 +0800

    add log4js for logging

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var log4js        = require('log4js');
 var express       = require('express');
 var path          = require('path');
 var favicon       = require('serve-favicon');
@@ -44,7 +45,11 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
+// app.use(logger('dev'));
+// replace morgan with the log4js connect-logger
+log4js.configure('config/log4js.json', { reloadSecs: 300 });
+app.use(log4js.connectLogger(log4js.getLogger("http"), { level: 'auto' }));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
